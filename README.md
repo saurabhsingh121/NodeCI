4dccfc53e41dba4f2ed1eb39cccfa821da361db5
Saurabh Singh <saurabhsingh121@gmail.com>
2020-03-13 01:04:18 +0530

    travis config

---
+++ README.md
@@ -0,0 +1,4 @@
+# AdvancedNodeStarter
+
+Starting project for a course on Advanced Node @ Udemy
+And this course is taught by Stephen Grider @ Udemy
---
+++ models/User.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  googleId: String,
+  displayName: String,
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  }
+});
+
+mongoose.model("User", userSchema);
